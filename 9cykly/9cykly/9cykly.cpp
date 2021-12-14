0b182885a4ae5e9324c917a360b50cc0e17beeee
GolDMichal <92848066+GolDMichal@users.noreply.github.com>
2021-12-14 13:47:40 +0100

    Add files via upload

---
+++ 9cykly/9cykly/9cykly.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sh = 0;
+    int co = 10;
+    do
+    {
+        cout << "Klop. Klop. Klop. Penny." << endl;
+        sh++;
+    } while (sh < co);
+    return 0;
+
+    
+}
+
